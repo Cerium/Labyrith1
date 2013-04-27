61df601079b0360361711777d5877cd208cc0b17
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2013-04-27 06:34:03 -0700

    access modifier of class configuration

--- Labyrinth1/Labyrinth1/Configuration.cs
+++ Labyrinth1/Labyrinth1/Configuration.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace Labyrinth
 {
-    class Configuration
+    public class Configuration
     {
+        public const int PLAY_FIELD_WIDTH = 7;
+        public const int PLAY_FIELD_HEIGHT = 7;
         public const string FILE_NAME = "scoreboard";
         public const int GAME_FIELD_SIZE = 6;
     }
