575627dab64228ac46b24b88585a43749c5f4a8d
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2013-05-14 02:27:14 +0300

    Some tests of class Player

---
+++ Labyrinth1/Labyrinth.Tests/PlayerTset.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Labyrinth.Tests
+{
+    [TestClass]
+    public class PlayerTset
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            Player player = new Player();
+            int actual = player.GetPosition.Col;
+            int expected = 3;
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
