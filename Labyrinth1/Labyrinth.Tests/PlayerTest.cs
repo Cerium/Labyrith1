ab0e067df0bb7212c55dabb0bbe85aa685aa83bd
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2013-05-14 02:32:21 +0300

    new test of player

--- Labyrinth1/Labyrinth.Tests/PlayerTest.cs
+++ Labyrinth1/Labyrinth.Tests/PlayerTest.cs
@@ -7,10 +7,20 @@ namespace Labyrinth.Tests
     public class PlayerTest
     {
         [TestMethod]
-        public void ConstructorTest()
+        public void ConstructorTest_Col_POsition()
         {
             Player player = new Player();
             int actual = player.GetPosition.Col;
+            int expected = 3;
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void ConstructorTest_Row_POsition()
+        {
+            Player player = new Player();
+            int actual = player.GetPosition.Row;
             int expected = 3;
             Assert.AreEqual(expected, actual);
 
