6c7d55da66d12fa51726d0cec504d49344d1d8c3
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2013-05-16 22:52:53 +0300

    Added more unit tests

---
+++ Labyrinth1/Labyrinth.Tests/ObjectRendererTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Labyrinth.Tests
+{
+    [TestClass]
+    public class ObjectRendererTest
+    {
+        //[TestMethod]
+        
+    }
+}
